8987420c4776442141b88b362d233fff2685961c
imk2o <imk2o@icloud.com>
2023-04-02 07:11:32 +0900

    Initial Commit

---
+++ AISensei/AISenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISenseiApp.swift
+//  AISensei
+//
+//  Created by k2o on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct AISenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
